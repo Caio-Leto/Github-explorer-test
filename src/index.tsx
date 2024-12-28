72018f2c54666db052d057e04fa90d45c6dbc480
Caio-Leto <caioletoxd@gmail.com>
2024-12-28 12:36:06 -0300

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {App} from './App';
+
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error("O elemento com id 'root' não foi encontrado no DOM.");
+}
+
+const root = ReactDOM.createRoot(rootElement); // React 18
+root.render(<App />);
